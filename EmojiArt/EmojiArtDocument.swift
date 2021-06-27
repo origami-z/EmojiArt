601a52a0038e04b04cc6ba53b8ef6289398c56f9
Zhihao Cui <davidcui2@me.com>
2021-06-27 23:06:48 +0100

    Lecture 10, 30min

---
+++ EmojiArt/EmojiArtDocument.swift
@@ -0,0 +1,84 @@
+//
+//  EmojiArtDocument.swift
+//  EmojiArt
+//
+//  Created by Zhihao Cui on 27/06/2021.
+//
+
+import SwiftUI
+
+class EmojiArtDocument: ObservableObject
+{
+    @Published private(set) var emojiArt: EmojiArtModel {
+        didSet {
+            if emojiArt.background != oldValue.background {
+                fetchBackgroundImageDataIfNecessary()
+            }
+        }
+    }
+    
+    init() {
+        emojiArt = EmojiArtModel()
+        emojiArt.addEmoji("😃", at: (-200, -100), size: 80)
+        emojiArt.addEmoji("🥶", at: (50, 100), size: 60)
+    }
+    
+    var emojis: [EmojiArtModel.Emoji] { emojiArt.emojis }
+    var background: EmojiArtModel.Background { emojiArt.background }
+    
+    @Published var backgroundImage: UIImage?
+    @Published var backgroundImageFetchStatus = BackgroundImageFetchStatus.idle
+    
+    enum BackgroundImageFetchStatus {
+        case idle
+        case fetching
+    }
+    
+    private func fetchBackgroundImageDataIfNecessary() {
+        backgroundImage = nil
+        switch emojiArt.background {
+        case .url(let url):
+            // fetch the url
+            backgroundImageFetchStatus = .fetching
+            DispatchQueue.global(qos: .userInitiated).async {
+                let imageData = try? Data(contentsOf: url)
+                DispatchQueue.main.async { [weak self] in
+                    // Makes sure previous slow loading url is not overwritten a newer faster one
+                    if self?.emojiArt.background == EmojiArtModel.Background.url(url) {
+                        self?.backgroundImageFetchStatus = .idle
+                        if imageData != nil {
+                            self?.backgroundImage = UIImage(data: imageData!)
+                        }
+                    }
+                }
+            }
+        case .imageData(let data):
+            backgroundImage = UIImage(data: data)
+        case .blank:
+            break
+        }
+    }
+    
+    // MARK: - Intents
+    
+    func setBackground(_ background: EmojiArtModel.Background) {
+        emojiArt.background = background
+    }
+    
+    func addEmoji(_ emoji: String, at location: (x: Int, y: Int), size: CGFloat) {
+        emojiArt.addEmoji(emoji, at: location, size: Int(size))
+    }
+    
+    func moveEmoji(_ emoji: EmojiArtModel.Emoji, by offset: CGSize) {
+        if let index = emojiArt.emojis.index(matching: emoji) {
+            emojiArt.emojis[index].x += Int(offset.width)
+            emojiArt.emojis[index].y += Int(offset.height)
+        }
+    }
+    
+    func scaleEmoji(_ emoji: EmojiArtModel.Emoji, by scale: CGFloat) {
+        if let index = emojiArt.emojis.index(matching: emoji) {
+            emojiArt.emojis[index].size = Int((CGFloat(emojiArt.emojis[index].size) * scale).rounded(.toNearestOrAwayFromZero))
+        }
+    }
+}
---
+++ EmojiArt/EmojiArtModel.Background.swift
@@ -0,0 +1,30 @@
+//
+//  EmojiArtModel.Background.swift
+//  EmojiArt
+//
+//  Created by Zhihao Cui on 27/06/2021.
+//
+
+import Foundation
+
+extension EmojiArtModel {
+    enum Background: Equatable {
+        case blank
+        case url(URL)
+        case imageData(Data)
+        
+        var url: URL? {
+            switch self {
+            case.url(let url): return url
+            default: return nil
+            }
+        }
+        
+        var imageData: Data? {
+            switch self {
+            case .imageData(let data): return data
+            default: return nil
+            }
+        }
+    }
+}
