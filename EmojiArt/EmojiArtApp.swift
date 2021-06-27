335bc4f2b713491f801b994cf6a50c7348028077
Zhihao Cui <davidcui2@me.com>
2021-06-27 18:13:15 +0100

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Zhihao Cui on 27/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
